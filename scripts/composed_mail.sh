66d8bc91f27535dc2cdfe467704dc93eeff442d3
Pavel Shvets <shvets.pavel@gmail.com>
2014-05-26 18:06:10 +0400

    optimized compose_mail script
    Former-commit-id: 3620c639682b12eded7a87dd2a61c8b6ca93bdb2

--- scripts/composed_mail.sh
+++ scripts/composed_mail.sh
@@ -8,78 +8,70 @@
 # $2 - subject
 # $3 - msg
 set -u
+cd "$(dirname "$0")"
 
 MTIME_LIMIT=30
-MAIL_LIMIT=100
+SEP_LIMIT=3
 
-# exit if empty recipient
-if [ -z "$1" ]; then
-	echo "error: empty recipient"
+# exit if have not got enough params
+if [ $# -ne 3 ]
+then
+	echo "error: wrong params"
     exit 1
 fi
 
-HASH=`echo $1 | md5sum| cut -f1 -d" "`
+HASH=`echo $1 | md5sum | cut -f1 -d" "`
+MAIL_DIR=/tmp/octo_$HASH
 
-LOCK_FILE=/tmp/octo_$HASH.lock
-OUT_FILE=/tmp/octo_$HASH.tmp
-SUBJ=Subject
+# assuming dir exists
+function add_mail
+{
+	echo -e "$SUBJ_STR: $2\n$3" > `mktemp --tmpdir=$MAIL_DIR`
+}
 
-# get lock
-lockfile $LOCK_FILE
+SUBJ_STR="Subject"
 
-# add to the buffer
-echo >> $OUT_FILE
-echo '***' >> $OUT_FILE
-echo >> $OUT_FILE
-echo $SUBJ : "$2" >> $OUT_FILE
-echo -e "$3" >> $OUT_FILE
-
-rm -f $LOCK_FILE
-
-sleep $MTIME_LIMIT
-sleep 2 # o_O # some addititional sleep to ensure correct time
-
-lockfile $LOCK_FILE
-
-cur_time=`date +%s`
-mod_time=`stat -c %Y $OUT_FILE`
-
-MAILS=`cat $OUT_FILE | grep $SUBJ | wc -l`
-
-DANGER=`cat $OUT_FILE | grep $SUBJ | grep DANGER | wc -l`
-CRITICAL=`cat $OUT_FILE | grep $SUBJ | grep CRITICAL | wc -l`
-WARNING=`cat $OUT_FILE | grep $SUBJ | grep WARNING | wc -l`
-INFO=`cat $OUT_FILE | grep $SUBJ | grep INFO | wc -l`
-RECOVER=`cat $OUT_FILE | grep $SUBJ | grep RECOVER | wc -l`
-
-# if it is true - probably i was last who changed the file
-# if i was not - it does not matter, i can check conditions anyway
-if [ $(($cur_time - $mod_time)) -gt $MTIME_LIMIT ]
+# if i am the owner
+if mkdir $MAIL_DIR
 then
+	add_mail "$1" "$2" "$3"
 
-# send all if the time file is too old and there are some data
-	if [[ "$MAILS" -gt 1 ]]
-	then
-		echo "sending - old data"
-		cat $OUT_FILE | mail -s "Octotron($MAILS): CRITICAL: $CRITICAL DANGER: $DANGER WARNING: $WARNING INFO: $INFO RECOVER: $RECOVER" "$1"
-		> $OUT_FILE
-	fi
+	sleep $MTIME_LIMIT
 
-# send if there is just one
-	if [[ "$MAILS" -eq 1 ]]
+	SEND_DIR=$MAIL_DIR.$$
+	mv $MAIL_DIR $SEND_DIR
+
+	COUNT=`ls -1 $SEND_DIR | wc -l`
+
+	if [[ $COUNT -le $SEP_LIMIT ]]
 	then
-		echo "sending - single"
-		subj=`sed -n '4p' $OUT_FILE`
-		msg=`tail -n+5 $OUT_FILE`
-		bash ./single_mail.sh "$1" "$subj" "$msg"
-		> $OUT_FILE
+		for file in $SEND_DIR/*
+		do
+			SUBJ=`head -n1 $file`
+			tail -n+2 $file | bash ./single_mail.sh "$1" "$SUBJ"
+		done
+
+	else
+		OUT_FILE=`mktemp`
+
+		for file in $SEND_DIR/*
+		do
+			echo >> $OUT_FILE
+			echo '***' >> $OUT_FILE
+			echo >> $OUT_FILE
+			cat $file >> $OUT_FILE
+		done
+
+		DANGER=`cat $OUT_FILE | grep $SUBJ_STR | grep DANGER | wc -l`
+		CRITICAL=`cat $OUT_FILE | grep $SUBJ_STR | grep CRITICAL | wc -l`
+		WARNING=`cat $OUT_FILE | grep $SUBJ_STR | grep WARNING | wc -l`
+		INFO=`cat $OUT_FILE | grep $SUBJ_STR | grep INFO | wc -l`
+		RECOVER=`cat $OUT_FILE | grep $SUBJ_STR | grep RECOVER | wc -l`
+
+		cat $OUT_FILE | bash ./single_mail.sh "Octotron($COUNT): CRITICAL: $CRITICAL DANGER: $DANGER WARNING: $WARNING INFO: $INFO RECOVER: $RECOVER" "$1"
+		rm $OUT_FILE
 	fi
-# send if there are too many data
-elif [[ "$MAILS" -gt $MAIL_LIMIT ]];
-then
-	echo "sending - many data"
-	cat $OUT_FILE | mail -s "Octotron($MAILS): CRITICAL: $CRITICAL DANGER: $DANGER WARNING: $WARNING INFO: $INFO RECOVER: $RECOVER" "$1"
-	> $OUT_FILE
+		rm -rf $SEND_DIR
+else
+	add_mail "$1" "$2" "$3"
 fi
-
-rm -f $LOCK_FILE
