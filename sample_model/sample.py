bd8eda21cdf6846ecc0b83a60c66d9aafa70b481
Pavel Shvets <shvets.pavel@gmail.com>
2014-10-14 15:17:13 +0400

    removed static from the sample
    Former-commit-id: 374a5a60012f915f961ec7384257d5b4155cc63e

--- sample_model/sample.py
+++ sample_model/sample.py
@@ -10,28 +10,29 @@ from sample_lib import *
 NODES = 4
 CPU_PER_NODE = 2
 
-# create 4 objects with "type" = "node"
-# assigne to them attributes from collectd library module
-nodes = CreateObjects(NODES
-	, { "const" : {"type" : "node"}}
-	, lib_collectd.node_module)
+# create 4 objects with "type" = "node" and assign
+# to them attributes from the collectd library module
+nodes = CreateObjects(NODES, lib_collectd.node_module
+	, { "const" : {"type" : "node"}})
 
 # assign to every object ip from the file
 CSVReader.Declare(nodes, "sample_model/ip.csv")
 
-# create 8 nodes and assign to them properties, declared in sample_lib.py
-cpus = CreateObjects(NODES * CPU_PER_NODE
+# create 8 nodes and assign to them properties
+# declared in sample_lib.py
+cpus = CreateObjects(NODES * CPU_PER_NODE, my_module
 	, {
 		"const" : (my_const, {"type" : "cpu"}),
 		"sensor" : my_sensor,
 		"var" : my_var,
 		"react" : my_react
-	}, my_module)
+	})
 
 # add attribute "lid" to each cpu, that equals
-# its position in list by modulo CPU_PER_NODE
-# 0, 1, 0, 1, 0, 1, 0, 1
+# its position in the list by modulo CPU_PER_NODE
+# i.e. 0, 1, 0, 1, 0, 1, 0, 1
 Enumerator.Sequence(cpus, "lid", CPU_PER_NODE)
 
-# connect every node with 2 cpus
+# connect every node with 2 cpus using
+# two links for connection with specified types
 EveryToChunks(nodes, cpus, ["contain", "chill"])
--- sample_model/sample_lib.py
+++ sample_model/sample_lib.py
@@ -7,7 +7,7 @@ from octopy import *
 my_module = {
 # declare constants
 	"const" : {
-		"_static_temp_max" : 70,
+		"temp_max" : 70,
 	},
 
 # declare sensors
@@ -17,13 +17,13 @@ my_module = {
 
 # declare varying attrbiutes
 	"var" : {
-		"temp_ok" : UpperArgThreshold("temperature", "_static_temp_max"),
+		"temp_ok" : UpperArgThreshold("temperature", "temp_max"),
 	},
 
 # declare reactions
 	"react" : {
 	# invoke the Danger() reaction if "temperature" becomes greater
-	# than "_static_temp_max"
+	# than "temp_max"
 	# invoke the Recover() reaction when "temperature" lowers
 		Equals("temp_ok", False).Repeatable() :
 			(Danger("very high cpu temperature", "temperature").PrintParent("type", "ip")
@@ -35,7 +35,7 @@ my_module = {
 # separate declaration
 #
 my_const = {
-		"_static_load_max" : 90,
+		"load_max" : 90,
 }
 
 # declare sensors
@@ -45,12 +45,12 @@ my_sensor = {
 
 # declare varying attrbiutes
 my_var = {
-	"load_ok" : UpperArgThreshold("avg_load", "_static_load_max")
+	"load_ok" : UpperArgThreshold("avg_load", "load_max")
 }
 
 my_react = {
 	# invoke the Warning() reaction when "avg_load" becomes greater
-	# than "_static_load_max" and stays so for 60 or more seconds
+	# than "load_max" and stays so for 60 or more seconds
 		Equals("load_ok", False).Delay(60) :
 			Warning("high cpu load for last minute", "avg_load").PrintParent("type", "ip")
 }
