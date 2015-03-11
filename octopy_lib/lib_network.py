7f59f2debe606f6f07a0014df442d1258f3486a0
Pavel Shvets <shvets.pavel.srcc@gmail.com>
2015-03-11 16:28:16 +0300

    add key option for network module

--- octopy_lib/lib_network.py
+++ octopy_lib/lib_network.py
@@ -1,6 +1,6 @@
 from octopy import *
 
-def PingModule(timeout = Minutes(10)):
+def PingModule(timeout = Minutes(10), key = "ip"):
 	return {
 		"sensor" : {
 			"ping" : Boolean(timeout),
@@ -8,16 +8,16 @@ def PingModule(timeout = Minutes(10)):
 
 		"react" : {
 			Equals("ping", False).Repeat(2) :
-				( Danger("tag", "NETWORK").Msg("loc", "{ip}")
+				( Danger("tag", "NETWORK").Msg("loc", "{" + key + "}")
 					.Msg("descr", "{type}: ping failed twice")
-					.Msg("msg"  , "{type}[{ip}]: ping failed twice")
-				, Recover("tag", "NETWORK").Msg("loc", "{ip}")
+					.Msg("msg"  , "{type}[{" + key + "}]: ping failed twice")
+				, Recover("tag", "NETWORK").Msg("loc", "{" + key + "}")
 					.Msg("descr", "{type}: ping ok")
-					.Msg("msg"  , "{type}[{ip}]: ping ok")),
+					.Msg("msg"  , "{type}[{" + key + "}]: ping ok")),
 		}
 	}
 
-def SshModule(timeout = Minutes(10)):
+def SshModule(timeout = Minutes(10), key = "ip"):
 	return {
 		"sensor" : {
 			"ssh" : Boolean(timeout),
@@ -25,16 +25,16 @@ def SshModule(timeout = Minutes(10)):
 
 		"react" : {
 			Equals("ssh", False).Repeat(2) :
-				( Danger("tag", "NETWORK").Msg("loc", "{ip}")
+				( Danger("tag", "NETWORK").Msg("loc", "{" + key + "}")
 					.Msg("descr", "{type}: ssh failed twice")
-					.Msg("msg"  , "{type}[{ip}]: ssh failed twice")
-				, Recover("tag", "NETWORK").Msg("loc", "{ip}")
+					.Msg("msg"  , "{type}[{" + key + "}]: ssh failed twice")
+				, Recover("tag", "NETWORK").Msg("loc", "{" + key + "}")
 					.Msg("descr", "{type}: ssh ok")
-					.Msg("msg"  , "{type}[{ip}]: ssh ok")),
+					.Msg("msg"  , "{type}[{" + key + "}]: ssh ok")),
 		}
 	}
 
-def SnmpModule(timeout = Minutes(10)):
+def SnmpModule(timeout = Minutes(10), key = "ip"):
 	return {
 		"sensor" : {
 			"snmp" : Boolean(timeout),
@@ -42,11 +42,11 @@ def SnmpModule(timeout = Minutes(10)):
 
 		"react" : {
 			Equals("snmp", False).Repeat(2) :
-				( Danger("tag", "INFRASTRUCTURE").Msg("loc", "{ip}")
+				( Danger("tag", "INFRASTRUCTURE").Msg("loc", "{" + key + "}")
 					.Msg("descr", "{type}: snmp check failed twice")
-					.Msg("msg"  , "{type}[{ip}]: snmp check failed twice")
-				, Recover("tag", "INFRASTRUCTURE").Msg("loc", "{ip}")
+					.Msg("msg"  , "{type}[{" + key + "}]: snmp check failed twice")
+				, Recover("tag", "INFRASTRUCTURE").Msg("loc", "{" + key + "}")
 					.Msg("descr", "{type}: snmp check ok")
-					.Msg("msg"  , "{type}[{ip}]: snmp check ok")),
+					.Msg("msg"  , "{type}[{" + key + "}]: snmp check ok")),
 		}
 	}
